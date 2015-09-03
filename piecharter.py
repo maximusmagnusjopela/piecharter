49e63680b11e47a1443a741a6ffa6783e1e573be
Jonathan Pelletier <maximusmagnusjopela@gmail.com>
2015-09-02 21:12:13 -0400

    Adding main script

---
+++ piecharter.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+import matplotlib.pyplot as plt
+import argparse
+
+def main():
+    """Creates a simple pie chart and saves it to .png file image"""
+    print('hello world')
+    return
+
+if __name__ == '__main__':
+    main()
