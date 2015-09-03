44eb4ddf31ff19848cb2ce73b42eb13082fb70cc
Jonathan Pelletier <maximusmagnusjopela@gmail.com>
2015-09-02 21:31:16 -0400

    Adding cmd line args parsing

--- piecharter.py
+++ piecharter.py
@@ -5,7 +5,34 @@ import argparse
 
 def main():
     """Creates a simple pie chart and saves it to .png file image"""
-    print('hello world')
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+            '-l',
+            '--labels',
+            help='labels for the slices of the chart',
+            nargs='+',
+            required=True
+            )
+
+    parser.add_argument(
+            '-s',
+            '--sizes',
+            help='relative sizes of the slices',
+            nargs='+',
+            required=True
+            )
+
+    parser.add_argument(
+            '-f',
+            '--filename',
+            help='path to the filename where the chart will be saved',
+            default='chart.png'
+            )
+
+    args = parser.parse_args()
+    print(args)
+
     return
 
 if __name__ == '__main__':
